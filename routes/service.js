f5e0cab2f65fadd8947e06f6f25861f733bef415
nick <sbryant31@gmail.com>
2016-11-15 16:42:38 -0700

    async

--- routes/service.js
+++ routes/service.js
@@ -1,10 +1,10 @@
 let Service = require('../models/service');
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
 
 module.exports = function(app, passport, helpers) {
-  app.get('/services', function(req, res) {
-    return Service.getAll()
-    .then((results) => {
-      return res.json(results);
-    })
-  });
+  app.get('/services', async(function(req, res) {
+    let services = await(Service.findAll())
+    return res.json(services);
+  }));
 }
