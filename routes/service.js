9884a96898847df3ffbe433eeff0815a64b3b474
nick <sbryant31@gmail.com>
2016-11-15 16:35:22 -0700

    services lose the auth

--- routes/service.js
+++ routes/service.js
@@ -1,7 +1,7 @@
 let Service = require('../models/service');
 
 module.exports = function(app, passport, helpers) {
-  app.get('/services', helpers.checkauth(passport), function(req, res) {
+  app.get('/services', function(req, res) {
     return Service.getAll()
     .then((results) => {
       return res.json(results);
