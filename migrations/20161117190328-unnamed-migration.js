45a188fbedd7f979918cf0632ac7db67fc4266b6
nick <sbryant31@gmail.com>
2016-11-17 20:53:20 -0700

    first migration

--- migrations/20161117190328-unnamed-migration.js
+++ migrations/20161117190328-unnamed-migration.js
@@ -2,48 +2,70 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-    return queryInterface.addColumn('action', 'input_example', Sequelize.JSON)
-    .then(function() {
-      return queryInterface.addColumn('action', 'output_example', Sequelize.JSON)
-    })
-    .then(function() {
-      return queryInterface.addColumn('action', 'input_content_type', Sequelize.STRING)
-    })
-    .then(function() {
-      return queryInterface.addColumn('action', 'query', Sequelize.JSON)
-    })
-    .then(function() {
-      return queryInterface.addColumn('action', 'output_content_type', Sequelize.STRING)
-    })
-    .then(function() {
-      return queryInterface.addColumn('action', 'headers', Sequelize.JSON)
-    })
+		queryInterface.createTable('action', {
+			service_id: {
+				type: Sequelize.INTEGER
+			},
+			schemes: {
+				type: Sequelize.JSON
+			},
+			headers: {
+				type: Sequelize.JSON
+			},
+			query: {
+				type: Sequelize.JSON
+			},
+			host: {
+				type: Sequelize.STRING
+			},
+			path: {
+				type: Sequelize.STRING
+			},
+			method: {
+				type: Sequelize.STRING
+			},
+			creator_user_id: {
+				type: Sequelize.INTEGER
+			},
+			service_name: {
+				type: Sequelize.STRING,
+			},
+			function_name: {
+				type: Sequelize.STRING,
+			},
+			type: {
+				type: Sequelize.STRING
+			},
+			input_content_type: {
+				type: Sequelize.STRING
+			},
+			output_content_type: {
+				type: Sequelize.STRING
+			},
+			version: {
+				type: Sequelize.STRING
+			},
+			description: {
+				type: Sequelize.TEXT
+			},
+			input: {
+				type: Sequelize.JSON
+			},
+			output: {
+				type: Sequelize.JSON
+			},
+			input_example: {
+				type: Sequelize.JSON
+			},
+			output_example: {
+				type: Sequelize.JSON
+			},
+			official: {
+				type: Sequelize.BOOLEAN
+			},
+		});
   },
 
   down: function (queryInterface, Sequelize) {
-    return queryInterface.removeColumn('action', 'input_example')
-    .then(function() {
-      return queryInterface.removeColumn('action', 'output_example')
-    })
-    .then(function() {
-      return queryInterface.removeColumn('action', 'headers')
-    })
-    .then(function() {
-      return queryInterface.removeColumn('action', 'query')
-    })
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
   }
 };
