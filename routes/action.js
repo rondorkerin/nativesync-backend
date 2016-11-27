19c3402a1d911a81f531eca9b0809f5914e5cd15
nick <sbryant31@gmail.com>
2016-11-26 18:35:56 -0700

    make async

--- routes/action.js
+++ routes/action.js
@@ -6,24 +6,24 @@ var await = require('asyncawait/await');
 
 module.exports = function(app, helpers) {
 
-  app.get('/actions', function(req, res) {
+  app.get('/actions', async(function(req, res) {
     let actions = await(Action.findAll())
     return res.json(actions);
-  });
+  }));
 
-  app.post('/action/:id/invoke', function(req, res) {
+  app.post('/action/:id/invoke', async(function(req, res) {
     let clientID = 1;
     let action = await(Action.findById(req.params['id']))
     let Request = require('../services/request')
     let output = new Request(clientID, action).send(req.body)
     return res.json(output);
-  });
+  }));
 
-  app.post('/action/:service/:function/invoke', function(req, res) {
+  app.post('/action/:service/:function/invoke', async(function(req, res) {
     let clientID = 1;
     let action = await(Action.findOne({ where: {service_name: req.params['service'], function_name: req.params['function']}}))
     let Request = require('../services/request')
     let output = new Request(clientID, action).send(req.body)
     return res.json(output);
-  });
+  }));
 }
