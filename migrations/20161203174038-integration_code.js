e455dae51e2bebf2cdd4a8a9faa1fecf3f3b4e15
nick <sbryant31@gmail.com>
2016-12-03 11:10:57 -0700

    query interface'

--- migrations/20161203174038-integration_code.js
+++ migrations/20161203174038-integration_code.js
@@ -5,7 +5,7 @@ var await = require('asyncawait/await')
 
 module.exports = {
   up: async(function (queryInterface, Sequelize) {
-    Sequelize.transaction(async(function() {
+    queryInterface.transaction(async(function() {
       await(queryInterface.addColumn('integration_code', 'integration_instance_id', Sequelize.BIGINT))
       await(queryInterface.addIndex('integration_code', ['integration_instance_id'] ,{indicesType: 'UNIQUE'}));
       await(queryInterface.removeColumn('integration_code', 'integration_id'));
