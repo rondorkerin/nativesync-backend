6542d0bc40109dbfbb3a165e25b869e4d41b2e14
nick <sbryant31@gmail.com>
2016-12-07 15:03:21 -0700

    add query

--- workers/integration_runner.js
+++ workers/integration_runner.js
@@ -9,9 +9,9 @@ module.exports = async(function() {
   console.log('integration runner worker is running');
   let integrationInstances = await(Models['IntegrationInstance'].findAll({where: {active: true}}))
   for (let integrationInstance of integrationInstances) {
+    let integration = await(integrationInstance.getIntegration());
     console.log('wouldve done stuff');
     /*
-    let integration = await(integrationInstance.getIntegration());
     let integrationCode = await(Models['IntegrationCode'].findOne({where: {integration_instance_id: integrationInstance.id}}))
     let client = await(integrationInstance.getClient());
     console.log('scheduling integration', integration.title, 'instance id', integrationInstance.id)
