0a5e72e6f73bbe888356bb9d336d9261580adf5f
nick <sbryant31@gmail.com>
2016-12-13 11:02:02 -0700

    no type

--- helpers/checkauth.js
+++ helpers/checkauth.js
@@ -3,7 +3,7 @@ module.exports = function(passport) {
   return function(type) {
     if (type == 'client') {
       return passport.authenticate('client', { session: false})
-    } else if (type == 'user') {
+    } else if (type == 'user' || !type) {
       return passport.authenticate('user', { session: false})
     }
   }
