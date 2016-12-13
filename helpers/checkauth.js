52f97f62eca74ab71ca55050181c7db03d1e04d3
nick <sbryant31@gmail.com>
2016-12-13 11:00:50 -0700

    checkauth

--- helpers/checkauth.js
+++ helpers/checkauth.js
@@ -2,9 +2,9 @@ var config = require('config');
 module.exports = function(passport) {
   return function(type) {
     if (type == 'client') {
-      return passport.authenticate('client', { session: false, failureRedirect: '/auth/failure' })
+      return passport.authenticate('client', { session: false})
     } else if (type == 'user') {
-      return passport.authenticate('user', { session: false, failureRedirect: '/auth/failure' })
+      return passport.authenticate('user', { session: false})
     }
   }
 };
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -5,17 +5,17 @@ const config = require('config');
 const express = require('express');
 
 module.exports = function(app, helpers) {
-	helpers.passport.use('client', new HeaderApiKeyStrategy({
-		header: 'Authorization', prefix: 'Api-Key ', session: false},
-		false,
-		async(function(apikey, done) {
-			var client = await(app.Models.Client.findOne({where: {api_key: apikey}}));
-			if (!client) {
-				return done('invalid client API key', null);
-			}
-			return done(null, client);
-		})
-	));
+  helpers.passport.use('client', new HeaderApiKeyStrategy({
+    header: 'Authorization', prefix: 'Api-Key ', session: false},
+    false,
+    async(function(apikey, done) {
+      var client = await(app.Models.Client.findOne({where: {api_key: apikey}}));
+      if (!client) {
+        return done('invalid client API key', null);
+      }
+      return done(null, client);
+    })
+  ));
 
   var v1Router = express.Router();
   app.use('/v1', v1Router);
