ef459d34d067a6d86b0fb5d1330e896b4ac79fa9
nick <sbryant31@gmail.com>
2016-12-14 12:57:17 -0700

    service auth routes and stuff

--- routes/internal/service.js
+++ routes/internal/service.js
@@ -5,7 +5,18 @@ var await = require('asyncawait/await');
 
 module.exports = (app, helpers) => {
   app.get('/services', async((req, res) => {
-    let services = await(Service.findAll())
+    let services = await(Service.findAll({order: 'name desc'}))
     return res.json(services);
+  }));
+
+ app.post('/services', helpers.checkauth('user'), async(function(req, res) {
+    let result;
+    if (req.body.id) {
+      await(Service.update(req.body, {where: {id: req.body.id}}))
+      result = await(Service.findById(req.body.id));
+    } else {
+      result = await(Service.create(req.body))
+    }
+    return res.json(result);
   }));
 }
---
+++ routes/internal/service_auth.js
@@ -0,0 +1,23 @@
+var Models = require('../../models');
+let Service = Models.Service;
+let ServiceAuth = Models.ServiceAuth;
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+
+module.exports = (app, helpers) => {
+  app.get('/service_auths', async((req, res) => {
+    let service_auths = await(ServiceAuth.findAll({where: {service_id: req.query.service_id}}))
+    return res.json(services);
+  }));
+
+ app.post('/service_auths', helpers.checkauth('user'), async(function(req, res) {
+    let result;
+    if (req.body.id) {
+      await(ServiceAuth.update(req.body, {where: {id: req.body.id}}))
+      result = await(ServiceAuth.findById(req.body.id));
+    } else {
+      result = await(ServiceAuth.create(req.body))
+    }
+    return res.json(result);
+  }));
+}
