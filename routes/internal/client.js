49c260130a95f26abeb43dc7244eebd620d4d6fe
nick <sbryant31@gmail.com>
2016-12-22 12:42:38 -0700

    client routes

--- routes/internal/client.js
+++ routes/internal/client.js
@@ -3,10 +3,33 @@ var Client = Models.Client;
 var async = require('asyncawait/async');
 
 module.exports = (app, helpers) => {
-  app.post('/clients', helpers.checkauth('user'), (req, res) => {
-    var client = req.body.client;
-    return Client.create(client).then((results) => {
-      return res.json({success: true});
-    })
+  app.post('/clients/upsert', helpers.checkauth('user'), function(req, res) {
+    let result;
+    let client = req.body.client;
+
+    try {
+      if (client.id) {
+        await(Client.update(client, {where: {id: client.id}}))
+        client = await(Client.findById(client.id));
+      } else {
+        client = await(Client.create(client))
+      }
+
+      await(client.addUser(req.user.id));
+
+      return res.json({client: client});
+    } catch(e) {
+      console.log('error', e);
+      return res.status(500).send(e);
+    }
+  });
+  app.get('/client/:id', helpers.checkauth('user'), (req, res) => {
+    // todo: lock this down (validate the partner_id in the filter)
+    var client = await(Client.findById(req.params.id))
+    if (client) {
+      return res.json({client: client});
+    } else {
+      return res.status(400).send('no such client');
+    }
   });
 }
