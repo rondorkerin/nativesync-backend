344fdc11c681d1938ca01d58f616198bab41f9b9
nick <sbryant31@gmail.com>
2016-12-23 13:36:16 -0700

    client auth route

--- routes/internal/client_auth.js
+++ routes/internal/client_auth.js
@@ -1,8 +1,27 @@
 var Models = require('../../models')
 var ClientAuth = Models.ClientAuth;
 var async = require('asyncawait/async');
+var await = require('asyncawait/await');
 
 module.exports = (app, helpers) => {
+  app.get('/client_auths', helpers.checkauth('user'), (req, res) => {
+    // todo: lock this down (validate the partner_id in the filter)
+		var serviceAuthIds = req.query.service_auth_ids;
+		var clientId = req.query.client_id;
+		try {
+			var clientAuths = await(Models.ClientAuth.findAll({where: {
+				client_id: clientId,
+				service_auth_id: {
+					$in: serviceAuthIds
+				}
+			}}));
+			return res.json({client_auths: clientAuths});
+		} catch(e) {
+			console.log('error', e);
+			return res.status(500).send(e);
+		}
+	});
+
   app.post('/auth_credentials', helpers.checkauth(), (req, res) => {
     var credentials = req.body.credentials;
     credentials.client_id = req.user.id;
