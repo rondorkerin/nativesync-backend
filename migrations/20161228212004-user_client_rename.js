01eb85c3941de7ac2645813b0d62510c318730e3
nick <sbryant31@gmail.com>
2016-12-28 14:22:04 -0700

    new mig

---
+++ migrations/20161228212004-user_client_rename.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var async = require('asyncawait/async')
+var await = require('asyncawait/await')
+
+module.exports = {
+  up: async(function (queryInterface, Sequelize) {
+    await(queryInterface.renameTable('user_client', 'user_organization'))
+    await(queryInterface.dropTable('user_partner'))
+  }),
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
