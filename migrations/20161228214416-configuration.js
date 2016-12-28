21333abbdf54f8770edf40bd03202935826239e7
nick <sbryant31@gmail.com>
2016-12-28 14:45:16 -0700

    integration config

---
+++ migrations/20161228214416-configuration.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var async = require('asyncawait/async')
+var await = require('asyncawait/await')
+
+module.exports = {
+  up: async(function (queryInterface, Sequelize) {
+    await(queryInterface.addColumn('integration', 'configuration', Sequelize.JSON))
+  }),
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
