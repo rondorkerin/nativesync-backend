a49cbbfaab4fc7e5abc0d3ebab6824c47d74cb17
nick <sbryant31@gmail.com>
2017-01-04 10:08:41 -0700

    deprecated stuff

--- migrations/20170103235933-deprecated_stuff.js
+++ migrations/20170103235933-deprecated_stuff.js
@@ -5,8 +5,8 @@ var await = require('asyncawait/await')
 
 module.exports = {
   up: async(function (queryInterface, Sequelize) {
-    //await(queryInterface.removeColumn( 'integration', 'required_service_auth_ids'))
-    //await(queryInterface.removeColumn( 'integration_code', 'integration_instance_id'))
+    await(queryInterface.removeColumn( 'integration', 'required_service_auth_ids'))
+    await(queryInterface.removeColumn( 'integration_code', 'integration_instance_id'))
     await(queryInterface.removeColumn( 'user', 'password'))
     await(queryInterface.removeColumn( 'organization_system_auth', 'hash'))
     await(queryInterface.removeColumn( 'organization', 'api_key'))
