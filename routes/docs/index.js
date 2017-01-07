5e7d775198365ea78e6320322f45003026119677
nick <sbryant31@gmail.com>
2017-01-07 12:55:20 -0700

    allow cors

--- routes/docs/index.js
+++ routes/docs/index.js
@@ -7,6 +7,13 @@ module.exports = function(app, helpers) {
   var docsRouter = express.Router();
   app.use('/docs', docsRouter);
 
+	// allow CORS
+	docsRouter.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	});
+
   docsRouter.get('/swagger.json', async ((req, res, next) => {
     var swaggerJson = fs.readFileSync('./docs/swagger.json', {encoding: 'utf8'});
     return res.send(swaggerJson);
