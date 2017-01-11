56ddc2b72615029b6cb584c1f39308580afe710e
nick <sbryant31@gmail.com>
2017-01-11 14:31:32 -0700

    syntax

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,7 @@
 module.exports = function(passport) {
   return {
     checkauth: require('./checkauth')(passport),
-    passport: passport
+    passport: passport,
     internalize: (string) => {
       // turn a string into an internalized version
       return string.toLowerCase().replace(new RegExp('[^a-zA-Z0-9_]+', 'g'), '_');
