58193742e0e57109725c5a676567a9e28b9db474
nick <sbryant31@gmail.com>
2017-01-10 20:39:38 -0700

    add asyncawait

--- routes/oauth/index.js
+++ routes/oauth/index.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var Models = require('../../models');
 var _ = require('underscore');
 var OAuth = require('oauth')
+var await = require('asyncawait/await');
+var async = require('asyncawait/async');
 
 module.exports = function(app, helpers) {
 
@@ -11,37 +13,35 @@ module.exports = function(app, helpers) {
     console.log('callback URL hit', req.body, req.params.service_auth_id);
   })
 
-  app.get('/oauth/authenticate/1.0/:service_auth_id', (req, res, next) => {
-    return Models.ServiceAuth.findById(req.params.service_auth_id)
-    .then((serviceAuth) => {
-      var callbackURL = `/oauth/callback/1.0/${serviceAuth.id}`;
-      var oa = new OAuth.OAuth(
-        serviceAuth.details.requestTokenUrl,
-        serviceAuth.details.accessTokenRequestUrl,
-        serviceAuth.details.consumerKey,
-        serviceAuth.details.consumerSecret,
-        "1.0",
-        callbackURL,
-        serviceAuth.details.signatureMethod
-      )
+  app.get('/oauth/authenticate/1.0/:service_auth_id', async((req, res, next) => {
+    var serviceAuth = await(Models.ServiceAuth.findById(req.params.service_auth_id))
+    var callbackURL = `/oauth/callback/1.0/${serviceAuth.id}`;
+    var oa = new OAuth.OAuth(
+      serviceAuth.details.requestTokenUrl,
+      serviceAuth.details.accessTokenRequestUrl,
+      serviceAuth.details.consumerKey,
+      serviceAuth.details.consumerSecret,
+      "1.0",
+      callbackURL,
+      serviceAuth.details.signatureMethod
+    )
 
-      console.log('getting oauth token', oa);
-      oa.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results) {
-        if(error) console.log('error :' + error)
-        else {
-          console.log('oauth_token :' + oauthToken)
-          console.log('oauth_token_secret :' + oauthTokenSecret)
-          serviceAuth.details.oauthTokenSecret = oauthTokenSecret;
-          serviceAuth.details.oauthToken = oauthToken;
-          await(serviceAuth.save());
-          console.log('requestoken results :', results)
-          console.log("Requesting access token")
-          oa.getOAuthAccessToken(oauthToken, oauthTokenSecret, function(error, oauthAccessToken, oauthAccessTokenSecret, results2) {
-            console.log('accesstoken results :', (results2))
-            console.log('acces token', oauthAccessToken, oauthAccessTokenSecret);
-          });
-        }
-      })
+    oa.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results) {
+      if (error) {
+        console.log('error :' + error)
+      } else {
+        console.log('oauth_token :' + oauthToken)
+        console.log('oauth_token_secret :' + oauthTokenSecret)
+        serviceAuth.details.oauthTokenSecret = oauthTokenSecret;
+        serviceAuth.details.oauthToken = oauthToken;
+        await(serviceAuth.save());
+        console.log('requestoken results :', results)
+        console.log("Requesting access token")
+        oa.getOAuthAccessToken(oauthToken, oauthTokenSecret, function(error, oauthAccessToken, oauthAccessTokenSecret, results2) {
+          console.log('accesstoken results :', (results2))
+          console.log('acces token', oauthAccessToken, oauthAccessTokenSecret);
+        });
+      }
     })
-  });
+  }));
 };
