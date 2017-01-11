84de735aa59214fbffe5e9e6db430a79a6e6ea08
nick <sbryant31@gmail.com>
2017-01-10 18:17:13 -0700

    oauth yo

--- routes/oauth/index.js
+++ routes/oauth/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var fs = require('fs');
 var Models = require('../../models');
 var _ = require('underscore');
-var OAuth = require('node-oauth').OAuth
+var OAuth = require('oauth').OAuth
 console.log('oauth', OAuth)
 
 module.exports = function(app, helpers) {
