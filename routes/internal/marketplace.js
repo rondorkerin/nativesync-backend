ab10de17f1faa6568779f43a645ac15f5d6431f2
Nathan Quarles <nateq314@gmail.com>
2017-01-11 11:45:52 -0500

    refactor marketplace-related routes into their own file. These routes will not require authentication.

---
+++ routes/internal/marketplace.js
@@ -0,0 +1,36 @@
+'use strict';
+const Models = require('../../models');
+const Integration = Models.Integration;
+
+module.exports = (app) => {
+
+  app.get('/marketplace/integrations', (req, res) => {
+    var filter = req.query;
+    Integration.findAll({
+      where: filter,
+      include: [
+        {model: Models.Service, as: 'Services'},
+        Models.Organization
+      ],
+      order: ['organization_id', 'title', ['version', 'desc']]
+    }).then(integrations => { return res.json({integrations: integrations}); });
+  });
+
+  app.get('/marketplace/integration/:id', (req, res) => {
+    // todo: lock this down (validate the organization_id in the filter)
+    Integration.findById(
+          req.params.id,
+          {include: [
+            {model: Models.Service, as: 'Services', include: [{model: Models.ServiceDefinition, as: 'ServiceDefinitions'}]},
+            Models.Organization
+          ]}
+    ).then(integration => integration ?
+      // if an integration matching the given id was found, return it
+      res.json({ integration }) :
+
+      // otherwise, return 40 not found
+      res.status(400).send('no such integration')
+    );
+  });
+
+};
