76466ad18fa906ef3c8b25c60e33b15c3f14a834
nick <sbryant31@gmail.com>
2017-01-11 18:45:12 -0700

    merge vars

--- helpers/index.js
+++ helpers/index.js
@@ -2,7 +2,7 @@ module.exports = function(passport) {
   return {
     checkauth: require('./checkauth')(passport),
     passport: passport,
-    mergeVariables: require('merge_variables'),
+    mergeVariables: require('./merge_variables'),
     internalize: (string) => {
       // turn a string into an internalized version
       return string.toLowerCase().replace(new RegExp('[^a-zA-Z0-9_]+', 'g'), '_');
