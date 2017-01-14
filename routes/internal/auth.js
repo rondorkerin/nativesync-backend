64c22b6ef898d544b1c94ee11092ad6794f79e30
Nathan Quarles <nateq314@gmail.com>
2017-01-14 11:23:39 -0500

    yay logout is now successfully returning http 204, now remove debug lines

--- routes/internal/auth.js
+++ routes/internal/auth.js
@@ -92,13 +92,9 @@ module.exports = (app, helpers) => {
 
   app.post('/auth/logout', helpers.checkauth('user'), (req, res) => {
     try {
-      console.log("GOT HERE: A");
       var systemAuth = await(Models.UserSystemAuth.findOne({where: {user_id: req.user.id}}))
-      console.log("GOT HERE: B");
       systemAuth.token = null;
-      console.log("GOT HERE: C");
       await(systemAuth.save());
-      console.log("GOT HERE: D");
       return res.status(204).send();
     } catch(e) {
       console.log('error', e);
