a398f99de641d7584fdaade55b6643c5f577f80a
nick <sbryant31@gmail.com>
2017-01-18 22:24:22 -0700

    .

--- routes/internal/integration_request.js
+++ routes/internal/integration_request.js
@@ -11,7 +11,7 @@ module.exports = (app, helpers) => {
   app.get('/integration_request/:id', helpers.checkauth('user'), (req, res) => {
     var integrationRequest = req.body.integrationRequest;
     try {
-      integrationRequest = await(IntegrationRequest.findById(req.path.id));
+      integrationRequest = await(IntegrationRequest.findById(req.params.id));
       return res.json({integrationRequest: integrationRequest});
     } catch(e) {
       console.log('error', e);
